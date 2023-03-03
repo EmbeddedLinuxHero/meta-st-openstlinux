618930a2819b370de95a613f8b04c19d12e084da
Van Son <thanh.duong.vs@gmail.com>
2023-03-03 11:15:02 +0700

    add test usb

--- recipes-graphics/wayland/weston-init/test.sh
+++ recipes-graphics/wayland/weston-init/test.sh
@@ -3,14 +3,17 @@
 echo "1. test led"
 ls /sys/class/leds
 
-echo "2. test uart"
+echo "2. test usb"
+lsusb
+
+echo "3. test uart"
 ls /dev/tty*
 
-echo "3. test i2c codec"
+echo "4. test i2c codec"
 i2cdetect -y 0
 
-echo "4. test i2c pmic"
+echo "5. test i2c pmic"
 i2cdetect -y 3
 
-echo "5. test audio"
+echo "6. test audio"
 aplay -D plughw:CARD=STM32MP15DK /home/root/somewhere.wav

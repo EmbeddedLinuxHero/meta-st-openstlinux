73f71ce7165d0a07ae6549f941a316d93b3ab51d
Van Son <thanh.duong.vs@gmail.com>
2023-03-07 14:04:25 +0700

    add reset

--- recipes-graphics/wayland/weston-init/test.sh
+++ recipes-graphics/wayland/weston-init/test.sh
@@ -1,25 +1,31 @@
 #!/bin/sh
 
-echo "1. test led"
+echo "1. test led:"
 ls /sys/class/leds
 
-echo "2. test usb"
+echo "2. test usb:"
 lsusb
 
-echo "3. test uart"
+echo "3. test uart:"
 ls /dev/tty*
 
-echo "4. test i2c codec"
+echo "4. test i2c codec:"
 i2cdetect -y 0
 
-echo "5. test i2c pmic"
+echo "5. test i2c pmic:"
 i2cdetect -y 3
 
-echo "6. test uSD Card"
+echo "6. test uSD Card:"
 cat /proc/partitions | grep mmcblk0
 
-echo "7. test ethernet"
+echo "7. test ethernet:"
 ifconfig
 
-echo "8. test audio"
-aplay -D plughw:CARD=STM32MP15DK /home/root/somewhere.wav
+echo "8. test audio:"
+#aplay -D plughw:CARD=STM32MP15DK /home/root/somewhere.wav
+amixer -q -D pulse sset Master 30%; aplay /home/root/somewhere.wav &
+
+echo "9. test 4G:"
+./test-4g
+
+echo "9. test done!"
